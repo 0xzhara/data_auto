10a21231248d637742b8c4e9ede7c879f3e50dbd
zrhra.eth [Jettokōsutā] <122523974+zrhraJETTOKOSUTA@users.noreply.github.com>
2024-06-30 00:27:47 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -4,18 +4,7 @@ curl -s https://raw.githubusercontent.com/zrhraJETTOKOSUTA/bash-nobi.sh/main/bas
 echo "Join the Airdrop Nobi Telegram channel: https://t.me/airdropnobi"
 sleep 5
 
-# Install NVM (Node Version Manager)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-
-# Load NVM
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Install Node.js
-nvm install node
+sudo apt update && sudo apt install nodejs && sudo apt install npm
 
 # Clone the bot-data-lake repository
 git clone https://github.com/Mnuralim/bot-data-lake.git
-
-# Change to the bot-data-lake directory
-cd bot-data-lake

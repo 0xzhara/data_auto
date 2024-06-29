7f1091c21bfb8545f10ecc754020b1c4b0119937
zrhra.eth [Jettokōsutā] <122523974+zrhraJETTOKOSUTA@users.noreply.github.com>
2024-06-30 00:13:31 +0700

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/zrhraJETTOKOSUTA/bash-nobi.sh/main/bash%20logo.sh | bash
+echo "Join the Airdrop Nobi Telegram channel: https://t.me/airdropnobi"
+sleep 6

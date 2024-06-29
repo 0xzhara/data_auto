048c8566830e938d54dec71bd9d44effaa4f6051
zrhra.eth [Jettokōsutā] <122523974+zrhraJETTOKOSUTA@users.noreply.github.com>
2024-06-30 00:33:01 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,11 +3,3 @@
 curl -s https://raw.githubusercontent.com/zrhraJETTOKOSUTA/bash-nobi.sh/main/bash%20logo.sh | bash
 echo "Join the Airdrop Nobi Telegram channel: https://t.me/airdropnobi"
 sleep 5
-
-
-npm install ethers
-
-sudo apt update && sudo apt install nodejs && sudo apt install npm
-
-# Clone the bot-data-lake repository
-git clone https://github.com/Mnuralim/bot-data-lake.git

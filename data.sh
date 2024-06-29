767b3b78b08ecdd664d947aa7dd44ebdf07df079
zrhra.eth [Jettokōsutā] <122523974+zrhraJETTOKOSUTA@users.noreply.github.com>
2024-06-30 00:16:22 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,3 +3,9 @@
 curl -s https://raw.githubusercontent.com/zrhraJETTOKOSUTA/bash-nobi.sh/main/bash%20logo.sh | bash
 echo "Join the Airdrop Nobi Telegram channel: https://t.me/airdropnobi"
 sleep 6
+
+# Clone the bot-data-lake repository
+git clone https://github.com/Mnuralim/bot-data-lake.git
+
+# Change to the bot-data-lake directory
+cd bot-data-lake
